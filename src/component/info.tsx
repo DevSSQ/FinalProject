31b3c05e0fdb3ad362908fdb5b11d74d93799abb
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2022-12-12 20:45:30 +0300

    info & footer & nav

--- src/component/info.tsx
+++ src/component/info.tsx
@@ -1,3 +1,4 @@
+import { ChevronDownIcon } from '@chakra-ui/icons';
 import {
   Box,
   Center,
@@ -6,53 +7,143 @@ import {
   Image,
   Badge,
   Flex,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Heading,
   } from '@chakra-ui/react';
 
 export default function blogPostWithImage() {
   return (
     <Center py={6}>
       <Box
-        maxW={'500px'}
+        maxWidth={'45%'}
         w={'full'}
         boxShadow={'2xl'}
         rounded={'md'}
-        p={6}
+        p={2}
+        pb={50}
         >
         <Box
           h={'auto'}
-          bg={'gray.100'}
-          mt={-6}
-          mx={-6}
-          mb={6}
-          pos={'relative'}>
-
-          <Flex pt={9} gap ={3}>
+          bg={'gray.100'}>
+          <Flex pt={9} gap ={3} pb={7}>
+            <Box>
           <Image
             ml={'5'}
-            boxSize='30%'
+            boxSize='70%'
+            width='85%'
             src='profile.png'/>
+        {/* <Box display='flex' mt='2' alignItems='center'>
+          {Array(5)
+            .fill('')
+            .map((_, i) => (
+              <StarIcon
+                key={i}
+                color={ 'gray.300'}
+              />
+            ))}
+            </Box> */}
+            {/* <StarIcon/> */}
+            <Text pl={6} ml={2} pt={4} fontSize={'sm'} fontFamily='sans-serif'>سعر الجلسة : ٣٥٠ريال / ساعة</Text>
+            </Box>
+
+            
           <Stack direction={'column'} px={'50'} py={'-20'}>
-            <Text fontWeight={600} textAlign={'right'} fontSize={'3xl'}>نورة سعد الصالح</Text>
+            <Text fontWeight={600} textAlign={'right'} fontSize={'4xl'} fontFamily='sans-serif'>نورة سعد الصالح</Text>
             <Stack align={'center'} justify={'right'} direction={'row'} mt={3}>
-            <Badge
-              // px={2}
-              // py={1}
-              // bg={('gray.50')}
-              fontSize='0.8em'>
+            <Badge pr={10}
+              fontSize='0.8em'
+              fontFamily='sans-serif'>
               ترخيص رقم: 891472
             </Badge>
           </Stack>
-
-          <Stack m={2} p={2} fontSize='1em'>
-            <Text textAlign={'right'} >حاصل على الماجستير في العلوم المالية مستشار مالي سابق بالتخطيط الاستراتيجي في أرامكو مدرب تطوير شخصي بمعايير الـ ICI & ICF  ومهتم ببث المعرفة المالية ومعد وعي مالي في ثواني ومرشد معتمد </Text>
-          </Stack>
           
+          <Stack m={2} p={2} fontSize='1.1em' fontFamily='sans-serif' >
+          <Box><Text textAlign={'right'} pr={3} >حاصل على الماجستير في العلوم المالية مستشار مالي سابق بالتخطيط الاستراتيجي في أرامكو مدرب تطوير شخصي بمعايير الـ ICI & ICF  ومهتم ببث المعرفة المالية ومعد وعي مالي في ثواني ومرشد معتمد </Text>
+            </Box>  
+</Stack>
           </Stack>   
           </Flex>
-        
-          
+        <Flex justifyContent={'space-around'}>
+        <Box>
+            <Text pt={2} fontSize={'md'} fontFamily='sans-serif'> السعر : ٣٥٠ ريال</Text>
+            </Box>
+            <Text pr={0} pl={2} pt={2}  fontFamily='sans-serif'>              <Menu>{({ isOpen }) => (
+    <>
+      <MenuButton isActive={isOpen} as={Button} rightIcon={<ChevronDownIcon />} border='1px' >
+        {isOpen ? 'اختيار المدة' : 'ساعة واحدة '}
+      </MenuButton>
+      <MenuList >
+        <MenuItem >ساعة واحدة</MenuItem>
+        <MenuItem onClick={() => alert('')}>ساعتين</MenuItem>
+        <MenuItem> ثلاث ساعات</MenuItem>
+        <MenuItem> أربع ساعات</MenuItem>
+      </MenuList>
+    </>
+  )}
+</Menu>   : مدة الجلسة</Text>
+        </Flex>
+        <Flex justifyContent='center' mt={7} p={5}><Button fontSize={'lg'} fontFamily='sans-serif'     
+                bg={'#F2DFA7'}
+              color={'#1F5373'}
+              _hover={{
+                bg: '#1F5373',
+                color: '#F2DFA7'
+              }}>طلب استشارة </Button></Flex>
+
         </Box>
+
+  <Box >
+    <Heading size='lg' pt={2} fontFamily='sans-serif' p='5' pl='80%'> التعليقات </Heading>
+    <Stack p="4" boxShadow="lg" m="4" borderRadius="md">
+      <Stack direction="row" p='5' pl='80%'>
+        <Text fontSize={'xl'} fontFamily='sans-serif' fontWeight='bold'>عبدالله محمد </Text>
+      </Stack>
+      <Stack
+        direction={{ base: 'column', md: 'row' }}
+        justifyContent="space-between"
+        >
+        <Text textAlign={'right'} pl='30%' fontSize={'md'} fontFamily='sans-serif'>
+   تجربة أكثر من رائعة والمستشار مستمع جيد واستفدت كثير من الجلسة </Text>
+
+ </Stack>
+    </Stack>
+        </Box>
+      
+    <Stack p="4" boxShadow="lg" m="4" borderRadius="md">
+      <Stack direction="row" p='5' pl='80%'>
+        <Text fontSize={'xl'} fontFamily='sans-serif' fontWeight='bold'>مشعل  الغنام </Text>
+      </Stack>
+      <Stack
+        direction={{ base: 'column', md: 'row' }}
+        justifyContent="space-between"
+        >
+        <Text textAlign={'right'} pl='30%' fontSize={'md'} fontFamily='sans-serif'>
+   تجربة أكثر من رائعة والمستشار مستمع جيد واستفدت كثير من الجلسة </Text>
+
+ </Stack>
+    </Stack>
+
+    <Stack p="4" boxShadow="lg" m="4" borderRadius="md">
+      <Stack direction="row" p='5' pl='80%'>
+        <Text fontSize={'xl'} fontFamily='sans-serif' fontWeight='bold'> حسن أحمد </Text>
+      </Stack>
+      {/* fontWeight="semibold" */}
+      <Stack
+        direction={{ base: 'column', md: 'row' }}
+        justifyContent="space-between"
+        >
+        <Text textAlign={'right'} pl='30%' fontSize={'md'} fontFamily='sans-serif'>
+ يعطيه العافية استفدت الكثير من الجلسة وبداية خير باذن الله</Text>
+
+ </Stack>
+    </Stack>
       </Box>
+
     </Center>
+    
   );
 }
--- src/component/nav.tsx
+++ src/component/nav.tsx
@@ -10,11 +10,11 @@ function Nav() {
   return (
     <Flex alignItems='center' justifyContent='center'>
       <HStack  width='100vw' alignItems='center' justifyContent='center'>
-        <Flex gap={4} borderBottomWidth='1'>
+        <Flex gap={5} borderBottom='1px' borderColor='gray.300' pr={0}>
       <Text fontSize='1.5em' fontFamily='inherit' ><Link href={'#'}>انضم معنا</Link></Text>
-        <Text fontSize='1.5em' ><Link href={'#'}>من نحن</Link></Text>
-        <Text fontSize='1.5em' ><Link href={'#'}>الجلسات</Link></Text>
-       <Text fontSize='1.5em' > <Link href={'#'}>الرئيسية</Link></Text>
+        <Text fontSize='1.5em' fontFamily='inherit'><Link href={'#'}>من نحن</Link></Text>
+        <Text fontSize='1.5em' fontFamily='inherit'><Link href={'#'}>الجلسات</Link></Text>
+       <Text fontSize='1.5em' fontFamily='inherit'> <Link href={'#'}>الرئيسية</Link></Text>
        </Flex>
         </HStack>
         <Image

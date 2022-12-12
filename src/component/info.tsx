4cc4976015efa6cfc0ec0ba1e7ee00b7c936844f
Fatimah-Almutairi <fatima.m.mutairi@gmail.com>
2022-12-12 16:38:03 +0300

    nav

--- src/component/info.tsx
+++ src/component/info.tsx
@@ -3,15 +3,16 @@ import {
   Center,
   Stack,
   Text,
-  Avatar,
+  Image,
   Badge,
+  Flex,
   } from '@chakra-ui/react';
 
 export default function blogPostWithImage() {
   return (
     <Center py={6}>
       <Box
-        maxW={'445px'}
+        maxW={'500px'}
         w={'full'}
         boxShadow={'2xl'}
         rounded={'md'}
@@ -25,32 +26,29 @@ export default function blogPostWithImage() {
           mb={6}
           pos={'relative'}>
 
-          <Stack direction={'column'} align={'left'}>
-          <Avatar
-            mt={'5'}
+          <Flex pt={9} gap ={3}>
+          <Image
             ml={'5'}
-            size={'xl'}
-            src={'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'}
-          />
-            
+            boxSize='30%'
+            src='profile.png'/>
           <Stack direction={'column'} px={'50'} py={'-20'}>
             <Text fontWeight={600} textAlign={'right'} fontSize={'3xl'}>نورة سعد الصالح</Text>
             <Stack align={'center'} justify={'right'} direction={'row'} mt={3}>
             <Badge
-              px={2}
-              py={1}
-              bg={('gray.50')}
-              fontWeight={'400'}>
+              // px={2}
+              // py={1}
+              // bg={('gray.50')}
+              fontSize='0.8em'>
               ترخيص رقم: 891472
             </Badge>
           </Stack>
 
-          <Stack>
+          <Stack m={2} p={2} fontSize='1em'>
             <Text textAlign={'right'} >حاصل على الماجستير في العلوم المالية مستشار مالي سابق بالتخطيط الاستراتيجي في أرامكو مدرب تطوير شخصي بمعايير الـ ICI & ICF  ومهتم ببث المعرفة المالية ومعد وعي مالي في ثواني ومرشد معتمد </Text>
           </Stack>
           
           </Stack>   
-          </Stack>
+          </Flex>
         
           
         </Box>
--- src/component/nav.tsx
+++ src/component/nav.tsx
@@ -2,25 +2,26 @@ import {
   Flex,
   HStack,
   Link,
-  Image
+  Image,
+  Text,
 } from '@chakra-ui/react';
 
 function Nav() {
   return (
-    <Flex
-   alignItems='center'
-   justifyContent='center'
-   >
-      <HStack  width='100vw' alignItems='center' justifyContent='end'>
-        <Link href={'#'}>انضم معنا</Link>
-        <Link href={'#'}>من نحن</Link>
-        <Link href={'#'}>الجلسات</Link>
-        <Link href={'#'}>الرئيسية</Link>
+    <Flex alignItems='center' justifyContent='center'>
+      <HStack  width='100vw' alignItems='center' justifyContent='center'>
+        <Flex gap={4} borderBottomWidth='1'>
+      <Text fontSize='1.5em' fontFamily='inherit' ><Link href={'#'}>انضم معنا</Link></Text>
+        <Text fontSize='1.5em' ><Link href={'#'}>من نحن</Link></Text>
+        <Text fontSize='1.5em' ><Link href={'#'}>الجلسات</Link></Text>
+       <Text fontSize='1.5em' > <Link href={'#'}>الرئيسية</Link></Text>
+       </Flex>
+        </HStack>
         <Image
-              boxSize='180px'
+              boxSize='160px'
               
               src='./levelup.png'></Image>
-      </HStack>
+      
       </Flex>
 
   )
